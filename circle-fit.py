998d79da47164572443aedd2925bec817b520d28
William Henney <will@henney.org>
2018-02-24 17:30:40 -0600

    Iterate for circle fit, including only points with |theta_c| < 30

--- circle-fit.py
+++ circle-fit.py
@@ -123,22 +123,34 @@ def find_theta(x, y, x0, y0, uvec):
 
 
 class FittedCircle(object):
-    def __init__(self, x, y, xs, ys, verbose=False):
+    def __init__(self, x, y, xs, ys, mask=None, verbose=False):
         self.x = x
         self.y = y
         self.xs = xs
         self.ys = ys
         self.verbose = verbose
-        self.results = fit_circle_to_xy(self.x, self.y)
-        if self.verbose:
-            print(results)
+        if mask is None:
+            # Use all the x, y points
+            self.mask = np.ones_like(x).astype(bool)
+        else:
+            # Restrict to certain x, y points
+            self.mask = mask
+        self.results = fit_circle_to_xy(self.x[self.mask], self.y[self.mask])
         self.r0 = self.xs, self.ys
         self.rc = self.results.x
-        self.Rc = mean_radius(self.x, self.y, *self.results.x)
+        self.Rc = mean_radius(self.x[self.mask], self.y[self.mask], *self.results.x)
         self.xihat = axis_unit_vector(self.r0, self.rc)
         self.R0 = apex_distance(self.r0, self.rc, self.Rc, self.xihat)
+        # Calculate the theta values for all points, regardless of the mask
         self.theta = find_theta(self.x, self.y, self.xs, self.ys, self.xihat)
         self.theta_c = find_theta(self.x, self.y, self.rc[0], self.rc[1], self.xihat)
+        if self.verbose:
+            print(self.results.message)
+            print("Star position:", self.r0)
+            print("Center position:", self.rc)
+            print("Radius of curvature:", self.Rc)
+            print("Axis unit vector:", self.xihat)
+            print("Apex distance:", self.R0)
         
 
 
@@ -151,7 +163,7 @@ def plot_solution(region_filename, fits_filename, plotfile, verbose=True):
     ax.imshow(hdu.data, origin='lower', vmin=2.8, vmax=3.5, cmap='viridis')
 
     xs, ys, x, y = get_arc_xy(region_filename, fits_filename)
-    c = FittedCircle(x, y, xs, ys, verbose=True)
+    cc = [FittedCircle(x, y, xs, ys, verbose=True)]
 
     # Size of viewport
     size = 150
@@ -165,17 +177,24 @@ def plot_solution(region_filename, fits_filename, plotfile, verbose=True):
         linewidths=0.5)
 
 
-    m = np.abs(c.theta_c) <= 30.0
+    for iter in range(6):
+        m = np.abs(cc[-1].theta_c) <= 30.0
+        cc.append(FittedCircle(x, y, xs, ys, mask=m, verbose=True))
+
     ax.scatter(x[m], y[m], s=30, color='r', zorder=2)
     ax.scatter(x[~m], y[~m], s=15, color='w', zorder=2)
-    circle = matplotlib.patches.Circle(c.rc, radius=c.Rc, ec='k', fc='none')
-    ax.add_patch(circle)
-    ax.plot(
-        [c.rc[0], c.rc[0] + 1.2*c.Rc*c.xihat[0]],
-        [c.rc[1], c.rc[1] + 1.2*c.Rc*c.xihat[1]],
-        ls="--", color='w', zorder=1,
-    )
-    ax.scatter(c.rc[0], c.rc[1], s=30, color='k', zorder=2)
+
+    colors = sns.color_palette("Oranges_r", n_colors=len(cc))
+    for c, color in zip(cc, colors):
+        ax.add_patch(
+            matplotlib.patches.Circle(c.rc, radius=c.Rc, ec=color, fc='none'))
+        ax.plot(
+            [c.rc[0], c.rc[0] + 1.2*c.Rc*c.xihat[0]],
+            [c.rc[1], c.rc[1] + 1.2*c.Rc*c.xihat[1]],
+            ls="--", color=color,
+        )
+        ax.scatter(c.rc[0], c.rc[1], s=30, color=color)
+
     ax.scatter(xs, ys, s=30, color='k', zorder=2)
 
     ra, dec = ax.coords
@@ -201,6 +220,13 @@ TEST_PLOT_FILE = 'plot-w000-400-ridge.pdf'
 
 if __name__ == "__main__":
 
+    try:
+        arc = str(sys.argv[1])
+        TEST_REGION_FILE = TEST_REGION_FILE.replace("ridge", arc)
+        TEST_PLOT_FILE = TEST_PLOT_FILE.replace("ridge", arc)
+    except:
+        pass
+    
     # Test with simple points
     print("### Simple Test")
     results = fit_circle_to_xy(*TESTDATA)
